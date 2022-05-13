d847835370038377ace55907ff9908a81ebf9fa9
Arvind <tarvind710@gmail.com>
2022-05-13 18:41:12 +0530

    Commit check

--- Api/BLOs/AccessoryPartProcessingBLO.cs
+++ Api/BLOs/AccessoryPartProcessingBLO.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace Api.BLOs
 {
+
     public class AccessoryPartProcessingBLO : IComponentProcessing, IAccessoryPartProcessingBLO
     {
           private readonly IMapper _mapper;
